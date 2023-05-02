12ad2d30954b7c9b4cc6b5a306806aad6e558932
MikhailZarubin <mikhail.zarubin.itmm@mail.ru>
2023-05-02 17:31:11 +0300

    Fixed graphics_builder.py (non finalized)

--- graphics_builder.py
+++ graphics_builder.py
@@ -1,9 +1,20 @@
 import numpy as np
+import tkinter as tk
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
+class Point2D:
+  def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Point3D:
+  def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
 if __name__ == '__main__':
-  fileName = 'data/point_0.txt'
+  fileName = 'data/point_test_0.txt'
   dimension = -1
   points = []
   try:
@@ -14,16 +25,15 @@ if __name__ == '__main__':
 
     del pointInfo[0]
 
-    for i in range(0, dimension):
-      points.append([])
-
     for point in pointInfo:
        coordinates = point.split(' ')
        if coordinates.__len__() == dimension:
-          coordinateNumber = 0
-          for coordinate in coordinates:
-            points[coordinateNumber].append(coordinate)
-            coordinateNumber+=1
+          if dimension == 2:
+            point2D= Point2D(coordinates[0], coordinates[1])
+            points.append(point2D)
+          elif dimension == 3:
+             point3D = Point3D(coordinates[0], coordinates[1], coordinates[2])
+             points.append(point3D)
     
     pointsFile.close()
   except FileNotFoundError:
@@ -37,12 +47,34 @@ if __name__ == '__main__':
     ax.scatter(x, y)
     plt.savefig('data/graphic_2D.png')
   elif dimension == 3:
-    x = np.array(points[0], dtype=float)
-    y = np.array(points[1], dtype=float)
-    z = np.array(points[2], dtype=float)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x, y, z)
-    plt.savefig('data/graphic_3D.png')
+    x = []
+    y = []
+    yDict = {}
+    for point3D in points:
+       if point3D.y not in yDict:
+          yDict[point3D.y] = []
+       extractZ = yDict[point3D.y]
+       extractZ.append(point3D.z)
+       yDict[point3D.y] = extractZ
+       if point3D.x not in x:
+          x.append(point3D.x)
+       if point3D.y not in y:
+          y.append(point3D.y)
+    x = np.array(x, dtype=float)
+    y = np.array(y, dtype=float)
+    xgrid, ygrid = np.meshgrid(x, y)
+
+    zgrid = ygrid.copy()
+    for i in range(0, yDict.values().__len__()):
+       zgrid[i] = np.array(list(yDict.values())[i], dtype=float)
+
+    fig = plt.figure(figsize =(16, 9)) 
+    ax = plt.axes(projection ='3d') 
+    my_cmap = plt.get_cmap('cool') 
+    surf = ax.plot_surface(xgrid, ygrid, zgrid, cmap = my_cmap, edgecolor ='none') 
+    fig.colorbar(surf, ax = ax, shrink = 0.7, aspect = 7) 
+    ax.set_title('Graphic') 
+    plt.show()
+    tk().mainloop() 
   else:
     print('Not supported visualization of a task with dimension is', dimension)
