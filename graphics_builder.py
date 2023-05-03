226b491dc034f13529d9beb37c5afca91665e9ba
MikhailZarubin <mikhail.zarubin.itmm@mail.ru>
2023-05-04 00:41:28 +0300

    Added algoritm points to graphics

--- graphics_builder.py
+++ graphics_builder.py
@@ -1,6 +1,9 @@
 import numpy as np
-import tkinter as tk
 import matplotlib.pyplot as plt
+from matplotlib import cm
+
+functionPointsPath = 'data/function_points/'
+algorithmPointsPath = 'data/algorithm_points/'
 
 class Point3D:
   def __init__(self, x, y, z):
@@ -8,43 +11,36 @@ class Point3D:
         self.y = y
         self.z = z
 
-if __name__ == '__main__':
-  fileName = 'data/point_test_0.txt'
-  dimension = -1
-  points = []
-  try:
-    pointsFile = open(fileName)
+def convertPoints3DtoArrays(points3D):
+  x = []
+  y = []
+  z = []
 
-    pointInfo = pointsFile.read().split('\n')
-    dimension = int(pointInfo[0])
-
-    del pointInfo[0]
-
-    for point in pointInfo:
-       coordinates = point.split(' ')
-       if coordinates.__len__() == dimension:
-          if dimension == 3:
-             point3D = Point3D(coordinates[0], coordinates[1], coordinates[2])
-             points.append(point3D)
+  for point3D in points3D:
+    x.append(point3D.x)
+    y.append(point3D.y)
+    z.append(point3D.z)
     
-    pointsFile.close()
-  except FileNotFoundError:
-    print('Non existing file with name', fileName)
+  x = np.array(x, dtype=float)
+  y = np.array(y, dtype=float)
+  z = np.array(z, dtype=float)
 
-if dimension == 3:
+  return x, y, z
+
+def convertPoints3Dto2DArrays(points3D):
   x = []
   y = []
   yDict = {}
-  for point3D in points:
+
+  for point3D in points3D:
     if point3D.y not in yDict:
        yDict[point3D.y] = []
-    extractZ = yDict[point3D.y]
-    extractZ.append(point3D.z)
-    yDict[point3D.y] = extractZ
+    yDict[point3D.y].append(point3D.z)
     if point3D.x not in x:
       x.append(point3D.x)
     if point3D.y not in y:
        y.append(point3D.y)
+
   x = np.array(x, dtype=float)
   y = np.array(y, dtype=float)
   xgrid, ygrid = np.meshgrid(x, y)
@@ -53,12 +49,64 @@ if dimension == 3:
   for i in range(0, yDict.values().__len__()):
      zgrid[i] = np.array(list(yDict.values())[i], dtype=float)
 
-  fig = plt.figure(figsize =(16, 9)) 
-  ax = plt.axes(projection ='3d') 
-  my_cmap = plt.get_cmap('cool') 
-  surf = ax.plot_surface(xgrid, ygrid, zgrid, cmap = my_cmap, edgecolor ='none') 
-  fig.colorbar(surf, ax = ax, shrink = 0.7, aspect = 7) 
+  return xgrid, ygrid, zgrid
+
+if __name__ == '__main__':
+  pointsfileName = 'points_2.txt'
+  dimension = -1
+  optimumPoint = []
+  functionPoints = []
+  algorithmPoints = []
+  try:
+    functionPointsFile = open(functionPointsPath + pointsfileName)
+    algorithmPointsFile = open(algorithmPointsPath + pointsfileName)
+
+    functionPointsInfo = functionPointsFile.read().split('\n')
+    algorithmPointsInfo = algorithmPointsFile.read().split('\n')
+    
+    dimension = int(functionPointsInfo[0])
+    del functionPointsInfo[0]
+
+    for functionPoint in functionPointsInfo:
+       coordinates = functionPoint.split(' ')
+       if coordinates.__len__() == dimension:
+          if dimension == 3:
+             point3D = Point3D(coordinates[0], coordinates[1], coordinates[2])
+             functionPoints.append(point3D)
+
+  
+    for algorithmPoint in algorithmPointsInfo:
+       coordinates = algorithmPoint.split(' ')
+       if coordinates.__len__() == dimension:
+          if dimension == 3:
+             point3D = Point3D(coordinates[0], coordinates[1], coordinates[2])
+             algorithmPoints.append(point3D)
+
+    optimumPoint.append(algorithmPoints[algorithmPoints.__len__() - 1])
+    algorithmPoints.remove(algorithmPoints[algorithmPoints.__len__() - 1])
+    
+    functionPointsFile.close()
+    algorithmPointsFile.close()
+  except FileNotFoundError:
+    print('Non existing file with name', functionPointsFile)
+
+if dimension == 3:
+  xgrid, ygrid, zgrid = convertPoints3Dto2DArrays(functionPoints)
+  x, y, z = convertPoints3DtoArrays(algorithmPoints)
+  optX, optY, optZ = convertPoints3DtoArrays(optimumPoint)
+
+  ax = plt.axes(projection='3d')             
+  
+  ax.plot_surface(xgrid, ygrid, zgrid, cmap = cm.cool) 
+  #ax.scatter(x, y, z, c = 'red')
+  ax.scatter(optX, optY, optZ, c='green')
+
   ax.set_title('Task of Global Optimization') 
+
+  ax.set_xlabel('X')
+  ax.set_ylabel('Y')
+  ax.set_zlabel('Z')
+
   plt.show()
 else:
   print('Not supported visualization of a task with dimension is', dimension)
