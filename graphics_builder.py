383226adce74b4995016661eaf9424e372290684
MikhailZarubin <mikhail.zarubin.itmm@mail.ru>
2023-05-03 23:05:03 +0300

    Finalized variant showing graphics

--- graphics_builder.py
+++ graphics_builder.py
@@ -2,11 +2,6 @@ import numpy as np
 import tkinter as tk
 import matplotlib.pyplot as plt
 
-class Point2D:
-  def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
 class Point3D:
   def __init__(self, x, y, z):
         self.x = x
@@ -28,10 +23,7 @@ if __name__ == '__main__':
     for point in pointInfo:
        coordinates = point.split(' ')
        if coordinates.__len__() == dimension:
-          if dimension == 2:
-            point2D= Point2D(coordinates[0], coordinates[1])
-            points.append(point2D)
-          elif dimension == 3:
+          if dimension == 3:
              point3D = Point3D(coordinates[0], coordinates[1], coordinates[2])
              points.append(point3D)
     
@@ -39,42 +31,34 @@ if __name__ == '__main__':
   except FileNotFoundError:
     print('Non existing file with name', fileName)
 
-  if dimension == 2:
-    x = np.array(points[0], dtype=float)
-    y = np.array(points[1], dtype=float)
-    fig = plt.figure()
-    ax = fig.add_subplot(11, projection='2d')
-    ax.scatter(x, y)
-    plt.savefig('data/graphic_2D.png')
-  elif dimension == 3:
-    x = []
-    y = []
-    yDict = {}
-    for point3D in points:
-       if point3D.y not in yDict:
-          yDict[point3D.y] = []
-       extractZ = yDict[point3D.y]
-       extractZ.append(point3D.z)
-       yDict[point3D.y] = extractZ
-       if point3D.x not in x:
-          x.append(point3D.x)
-       if point3D.y not in y:
-          y.append(point3D.y)
-    x = np.array(x, dtype=float)
-    y = np.array(y, dtype=float)
-    xgrid, ygrid = np.meshgrid(x, y)
+if dimension == 3:
+  x = []
+  y = []
+  yDict = {}
+  for point3D in points:
+    if point3D.y not in yDict:
+       yDict[point3D.y] = []
+    extractZ = yDict[point3D.y]
+    extractZ.append(point3D.z)
+    yDict[point3D.y] = extractZ
+    if point3D.x not in x:
+      x.append(point3D.x)
+    if point3D.y not in y:
+       y.append(point3D.y)
+  x = np.array(x, dtype=float)
+  y = np.array(y, dtype=float)
+  xgrid, ygrid = np.meshgrid(x, y)
 
-    zgrid = ygrid.copy()
-    for i in range(0, yDict.values().__len__()):
-       zgrid[i] = np.array(list(yDict.values())[i], dtype=float)
+  zgrid = ygrid.copy()
+  for i in range(0, yDict.values().__len__()):
+     zgrid[i] = np.array(list(yDict.values())[i], dtype=float)
 
-    fig = plt.figure(figsize =(16, 9)) 
-    ax = plt.axes(projection ='3d') 
-    my_cmap = plt.get_cmap('cool') 
-    surf = ax.plot_surface(xgrid, ygrid, zgrid, cmap = my_cmap, edgecolor ='none') 
-    fig.colorbar(surf, ax = ax, shrink = 0.7, aspect = 7) 
-    ax.set_title('Graphic') 
-    plt.show()
-    tk().mainloop() 
-  else:
-    print('Not supported visualization of a task with dimension is', dimension)
+  fig = plt.figure(figsize =(16, 9)) 
+  ax = plt.axes(projection ='3d') 
+  my_cmap = plt.get_cmap('cool') 
+  surf = ax.plot_surface(xgrid, ygrid, zgrid, cmap = my_cmap, edgecolor ='none') 
+  fig.colorbar(surf, ax = ax, shrink = 0.7, aspect = 7) 
+  ax.set_title('Task of Global Optimization') 
+  plt.show()
+else:
+  print('Not supported visualization of a task with dimension is', dimension)
